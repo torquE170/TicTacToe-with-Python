f20b00a673e27398070904d53c0e4491eaf25740
Eduard Jereghi <ntorque170@gmail.com>
2025-02-07 17:53:22 +0200

    Incorrectly placing more than one symbol when blocking opponent.

--- game.py
+++ game.py
@@ -257,11 +257,7 @@ class Game:
 
                     # Check for blocking move
                     for i in range(3):
-                        if added:
-                            break
                         for j in range(3):
-                            if added:
-                                break
                             if self.is_empty((i, j)):
                                 self.add_element(self.other_symbol(), (i, j))
                                 if self.check_status(True) == f">> {self.other_symbol()} won!":
@@ -272,6 +268,7 @@ class Game:
                                 self.remove_element((i, j))  # Undo move
                         else:
                             continue
+                        break
                     if added:
                         break
 
